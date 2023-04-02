bd689328336dc884a26354218ecf2a418a984b8e
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2023-04-03 01:02:40 +0900

    fix: cleanup func

--- internal/usecase/repo/avatar_test.go
+++ internal/usecase/repo/avatar_test.go
@@ -45,12 +45,12 @@ func TestAvatarRepo_Create(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer func() {
+	defer func(id int64) {
 		err = repo.Delete(avatar.ID)
 		if err != nil {
 			t.Log(err)
 		}
-	}()
+	}(avatar.ID)
 
 	got, err := repo.GetByID(avatar.ID)
 	if err != nil {
@@ -69,12 +69,12 @@ func TestAvatarRepo_Create(t *testing.T) {
 		t.Error(err)
 		return
 	}
-	defer func() {
+	defer func(id int64) {
 		err = repo.Delete(avatar.ID)
 		if err != nil {
 			t.Log(err)
 		}
-	}()
+	}(avatar.ID)
 
 	got, err = repo.GetByID(avatar.ID)
 	if err != nil {
